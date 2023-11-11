503e1914f4fc306196b64be9cbb20c27d2beaded
essaket <hichamessaket17gmail.com>
2023-11-11 11:24:40 -0800

    Done get_function.c

---
+++ get_function.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * get_function - function specifiers.
+ *
+ * @specifier: Conversion specifiers.
+ * @args: arguments.
+ *
+ * Return: char count.
+ */
+
+int get_function(char specifier, va_list args)
+{
+	int i = 0;
+	int counter = 0;
+
+	parametres_p spec[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_perc},
+		{'d', print_digit},
+		{'i', print_digit},
+		{0, NULL}
+	};
+
+	while (spec[i].s)
+	{
+		if (specifier == spec[i].s)
+			counter += spec[i].f(args);
+		i++;
+	}
+
+	if (counter == 0)
+	{
+		counter += _putchar('%');
+		counter += _putchar(specifier);
+	}
+
+	return (counter);
+}
