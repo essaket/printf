4a396ec9ed7a63249434f3b163c1ef5cde8eeddc
essaket <hichamessaket17gmail.com>
2023-11-11 11:28:17 -0800

    Done print_string.c

---
+++ print_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_string - print string.
+ *
+ * @args: argument.
+ *
+ * Return: character count.
+ */
+
+int print_string(va_list args)
+{
+	int i;
+	int counter = 0;
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(null)";
+
+	if (str[0] == '\0')
+		return (-1);
+
+	for (i = 0; str[i] != '\0'; i++)
+		counter+= _putchar(str[i]);
+
+	return (counter);
+}
