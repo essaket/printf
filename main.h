b1b8318b57be86998d9a44fc9689888804d4be68
4zeddine <azeddine.nader@gmail.com>
2023-11-11 10:36:52 -0800

    adding main.h file

--- main.h
+++ main.h
@@ -1,6 +1,31 @@
-#ifndef PRINTF_H
-#define PRINTF_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct parametres - struct for printer functions
+ * @type_arg: identifier
+ * @f: pointer to a printer functions
+ *
+ * Description: struct that stores pointers to a
+ * printer functions.
+ */
+typedef struct parametres
+{
+	char s;
+	int (*f)(va_list);
+} parametres_p;
 
 int _printf(const char *format, ...);
+int get_function(char s, va_list args);
+int _putchar(char c);
+int print_char(va_list args);
+int print_digit(va_list args);
+int print_perc(va_list args);
+int print_string(va_list args);
 
-#endif 
+#endif
