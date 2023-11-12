e79b46518e0cf90e8e1918af05092440d2ca7699
4zeddine <azeddine.nader@gmail.com>
2023-11-12 13:34:13 -0800

    edditing files by Azeddine

--- get_function.c
+++ get_function.c
@@ -1,40 +1,29 @@
 #include "main.h"
-
 /**
- * get_function - getting function specifiers.
- *
- * @c: Conversion specifiers.
- * @args: arguments.
- *
- * Return: char count.
- */
-
-int get_function(char c, va_list args)
+  * get_function - Prints a valid specifier
+  * @chara: The specifier to prints
+  * @args: A list of variadic arguments
+  *
+  * Return: The length of the specifier
+  */
+int get_function(char chara, va_list args)
 {
-	int i = 0;
-	int count_fun = 0;
-
-	parametres_p specifiers[] = {
-		{'c', print_char},
-		{'s', print_string},
-		{'d', print_digit},
-		{'i', print_digit},
-		{'%', print_perc},
-		{0, NULL}
+	int i  = 0, count = 0;
+	specifiers_p specifiers[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_digit},
+		{"i", print_digit},
+		{NULL, NULL}
 	};
 
 	while (specifiers[i].specifier)
 	{
-		if (c == specifiers[i].specifier)
-			count_fun += specifiers[i].f(args);
+		if (*specifiers[i].specifier == chara)
+			count = specifiers[i].f(args);
+
 		i++;
 	}
 
-	if (count_fun == 0)
-	{
-		count_fun += _putchar('%');
-		count_fun += _putchar(c);
-	}
-
-	return (count_fun);
+	return (count);
 }
