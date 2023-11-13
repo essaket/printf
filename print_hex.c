c32273c30b4db14055ad6746326fbe2df1d4b9ab
4zeddine <azeddine.nader@gmail.com>
2023-11-13 09:04:44 -0800

    adding file by Azeddine

---
+++ print_hex.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_hex - prints an hexadicimal number.
+ * @args: arguments list.
+ * Return: integer of counter.
+ */
+int print_hex(va_list args)
+{
+	int i, counter = 0, *array;
+	unsigned int a, b;
+
+	a = va_arg(args, unsigned int);
+	b = a;
+	while (a / 16 != 0)
+	{
+		a /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = b % 16;
+		b /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
