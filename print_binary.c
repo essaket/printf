c4f4e113575f826b0bfb443064e3a082b4d811ed
essaket <hichamessaket17gmail.com>
2023-11-13 06:16:00 -0800

    edditing by essaket

--- print_binary.c
+++ print_binary.c
@@ -1,23 +1,47 @@
 #include "main.h"
 
 /**
- * print_binary - prints a binary
- * 
- * @args: list of arguments
- * 
- * Return: a number of binary number
-*/
+  * recursion_binary - Prints a binary
+  * @n: integer to print
+  *
+  */
+
+void recursion_binary(int n)
+{
+	unsigned int i;
+
+	i = n;
+	if (i / 2)
+		recursion_binary(i / 2);
+	_putchar(i % 2 + '0');
+}
+
+/**
+  * print_binary - Prints a binary
+  * @args: A list of variadic arguments
+  *
+  * Return: The number of binary digits
+  */
 
 int print_binary(va_list args)
 {
-    unsigned int number = va_arg(args, int);
-    int i = 0;
+	unsigned int a = 0;
+	int b = 0, c = 0;
 
-    while (number!= 0)
-    {
-        _putchar(number % 2 + '0');
-        number /= 2;
-        i++;
-    }
-    return (i);
+	c = va_arg(args, int);
+	a = c;
+	if (c < 0)
+	{
+		_putchar('1');
+		c = c * -1;
+		a = c;
+		b += 1;
+	}
+	while (a > 0)
+	{
+		a = a / 2;
+		b++;
+	}
+	recursion_binary(c);
+	return (b);
 }
