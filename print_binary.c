7ecbb6130504763636336d7d433dc0d0551e3248
essaket <hichamessaket17gmail.com>
2023-11-13 09:24:49 -0800

    edditing by essaket

--- print_binary.c
+++ print_binary.c
@@ -2,31 +2,36 @@
 /**
 * print_binary - Prints an unsigned number
 * @args: Lista of arguments
-
+*
 * Return: Numbers of char printed.
 */
+
 int print_binary(va_list args)
 {
-unsigned int n, m, i, sum;
-unsigned int a[32];
-int count;
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
 
-n = va_arg(args, unsigned int);
-m = 2147483648; /* (2 ^ 31) */
-a[0] = n / m;
-for (i = 1; i < 32; i++)
-{
-    m /= 2;
-    a[i] = (n / m) % 2;
-}
-for (i = 0, sum = 0, count = 0; i < 32; i++){
-    sum += a[i];
-    if (sum || i == 31)
-    {
-    char z = '0' + a[i];
-    _putchar(z);
-    count++;
-    }
-}
-return (count);
+	n = va_arg(args, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			_putchar(z);
+			count++;
+		}
+	}
+
+	return (count);
 }
