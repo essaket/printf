6671eea3bf63808f19f4ddf5d3c258ba8b71af9b
essaket <hichamessaket17gmail.com>
2023-11-14 11:39:42 -0800

    edditing by essaket

---
+++ print_rot13.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * printf_rot13 - printf string to rot13
+ *
+ * @args: input arguments
+ *
+ * Return: an int
+ */
+
+int printf_rot13(va_list args)
+{
+	int i, j, nmb = 0, k = 0;
+	char *pnt = va_arg(args, char*);
+	char array1[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char array2[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	if (pnt == NULL)
+		pnt = "(null)";
+	for (i = 0; pnt[i]]; i++)
+	{
+		k = 0;
+		for (j = 0; array1[j] && !k; j++)
+		{
+			if (s[i] == array1[j])
+			{
+				_putchar(array2[j]);
+				nmb++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(pnt[i]);
+			nmb++;
+		}
+	}
+	return (nmb);
+}
