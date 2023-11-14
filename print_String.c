5865c8ab7ffd5913b5fb42425f643686c794aa97
essaket <hichamessaket17gmail.com>
2023-11-14 09:35:52 -0800

    edditing by essaket

---
+++ print_String.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * print_String - function that print a String
+ *
+ * @args: input argument
+ *
+ * Return: an int
+*/
+
+int print_String(va_list args)
+{
+	char *s;
+	int i, len = 0;
+	int cast;
+
+	s = va_arg(args, char *);
+	if (s != NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len = len + 2;
+			cast = s[i];
+			if (cast < 16)
+			{
+				_putchar('0');
+				len++;
+			}
+			len = len + printf_hex_plus(cast);
+		}
+		else
+		{
+			_putchar(s[i]);
+			len++;
+		}
+	}
+	return (len);
+	}
+
+	_putchar('(');
+	_putchar('n');
+	_putchar('u');
+	_putchar('l');
+	_putchar('l');
+	_putchar(')');
+	return (6);
+}
---
+++ print_hex_plus.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_hex_plus - prints an hexadecimal number.
+ * @n: arguments list.
+ * Return: counter.
+ */
+int print_hex_plus(unsigned long int n)
+{
+	long int i, counter = 0, *array;
+	unsigned long int a;
+
+	a = n
+	while (n / 16 != 0)
+	{
+		n /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(long int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = a % 16;
+		a = a / 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
